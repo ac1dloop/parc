eee2471274ce92edd390ea02c6ee15a93588e93f
ac1dloop <rakivvode@gmail.com>
2022-08-13 09:38:29 +0300

    added first test

--- src/main.c
+++ src/main.c
@@ -5,17 +5,11 @@ int main(int argc, char **argv){
 
     Item* head = parc_parse(argc, argv);
 
-    print_item_list(head);
+    int port = parc_get_int(head, "port");
 
-    int a = parc_get_int(head, "port");
+    printf("port: %d\n", port);
 
-    printf("got port: %d\n", a);
-    
-    int b = parc_get_int(head, "gaga");
-
-    printf("b: %d\n", b);
-
-    int c = parc_get_int(head, "no such value");
+    parc_free(head);
 
     return 0;
 }
